0507d94816993df825a3ad2365ad553be82711da
nicolaslangley <nicolasqlangley@gmail.com>
2013-11-20 20:47:45 -0500

    Updated main file with code to run basic stitching

--- Comp558-Project/main.cpp
+++ Comp558-Project/main.cpp
@@ -6,20 +6,99 @@
 //  Copyright (c) 2013 Nicolas Langley. All rights reserved.
 //
 
-#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <fstream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/stitching/stitcher.hpp"
+#include <opencv2/core/core.hpp>
 
-int main() {
-    char* windowName = "HelloWorldWindow";
-    cvNamedWindow(windowName,0x01);
-    IplImage* img = cvCreateImage(cvSize(0x96,0x32),IPL_DEPTH_8U,0x01);
-    CvFont font;
-    double hScale = 0x01;
-    double vScale = 0x01;
-    int lineWidth = 0x01;
-    cvInitFont(&font,CV_FONT_HERSHEY_COMPLEX_SMALL|CV_AA,hScale,vScale,0x00,lineWidth);
-    cvPutText(img,"Hello World.",cvPoint(0x00,0x1E),&font,cvScalar(0x00,0x00,0x00));
-    cvShowImage(windowName,img);
-    cvWaitKey();
-    return 0x00;
+using namespace std;
+using namespace cv;
+
+bool try_use_gpu = false;
+vector<Mat> imgs;
+string result_name = "result.jpg";
+
+void printUsage();
+int parseCmdArgs(int argc, char** argv);
+
+int main(int argc, char* argv[])
+{
+    int retval = parseCmdArgs(argc, argv);
+    if (retval) return -1;
+    
+    Mat pano;
+    Stitcher stitcher = Stitcher::createDefault(try_use_gpu);
+    Stitcher::Status status = stitcher.stitch(imgs, pano);
+    
+    if (status != Stitcher::OK)
+    {
+        cout << "Can't stitch images, error code = " << status << endl;
+        return -1;
+    }
+    
+    imwrite(result_name, pano);
+    return 0;
+}
+
+
+void printUsage()
+{
+    cout <<
+    "Rotation model images stitcher.\n\n"
+    "stitching img1 img2 [...imgN]\n\n"
+    "Flags:\n"
+    "  --try_use_gpu (yes|no)\n"
+    "      Try to use GPU. The default value is 'no'. All default values\n"
+    "      are for CPU mode.\n"
+    "  --output <result_img>\n"
+    "      The default is 'result.jpg'.\n";
+}
+
+
+int parseCmdArgs(int argc, char** argv)
+{
+    if (argc == 1)
+    {
+        printUsage();
+        return -1;
+    }
+    for (int i = 1; i < argc; ++i)
+    {
+        if (string(argv[i]) == "--help" || string(argv[i]) == "/?")
+        {
+            printUsage();
+            return -1;
+        }
+        else if (string(argv[i]) == "--try_gpu")
+        {
+            if (string(argv[i + 1]) == "no")
+                try_use_gpu = false;
+            else if (string(argv[i + 1]) == "yes")
+                try_use_gpu = true;
+            else
+            {
+                cout << "Bad --try_use_gpu flag value\n";
+                return -1;
+            }
+            i++;
+        }
+        else if (string(argv[i]) == "--output")
+        {
+            result_name = argv[i + 1];
+            i++;
+        }
+        else
+        {
+            Mat img = imread(argv[i]);
+            if (img.empty())
+            {
+                cout << "Can't read image '" << argv[i] << "'\n";
+                return -1;
+            }
+            imgs.push_back(img);
+        }
+    }
+    return 0;
 }
 
